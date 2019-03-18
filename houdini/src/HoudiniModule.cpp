4c9e66654801895ce583fab2e07351a42795f095
Liang Liu <qq79402005@gmail.com>
2019-03-18 13:36:15 +0800

    Houdini plugin need use release version engine library #11

--- houdini/src/HoudiniModule.cpp
+++ houdini/src/HoudiniModule.cpp
@@ -1,20 +1,10 @@
 #include "HoudiniModule.h"
 #include "engine/core/memory/MemAllocDef.h"
 
-#include <ft2build.h>
-#include FT_FREETYPE_H
-
 namespace Echo
 {
 	void registerModules()
 	{
-		//REGISTER_MODULE(HoudiniModule)
-		FT_Library m_library;
-		FT_Error result = FT_Init_FreeType(&m_library);
-		if (result == FT_Err_Ok)
-		{
-
-		}
 	}
 
     HoudiniModule::HoudiniModule()
